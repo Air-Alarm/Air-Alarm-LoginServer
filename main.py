0ec682601b3384fa6d4b7a313efed847df8522a9
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2021-09-12 14:16:06 +0900

    테이블 생성 수정

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ app = Flask(__name__, static_url_path='/static')
 app.secret_key = b'_5#aby2L"F4Q8z\n\xec]'
 
 def signup_db(id, pw, email, sn):
-    #conn.execute('CREATE TABLE sign(ID TEXT, PW TEXT, EMAIL TEXT, SN TEXT)')
+    try:
+        conn.execute('CREATE TABLE sign(ID TEXT, PW TEXT, EMAIL TEXT, SN TEXT)')
+    except Exception as e:
+        print("already", e)
     cur.execute('INSERT INTO sign VALUES (?, ?, ?, ?)', (id, pw, email, sn))
     conn.commit()
     print("good", id, pw, email, sn)
 
 @app.route('/signup_confirm', methods=['POST', 'GET'])
 def signup_confirm():
-    user = request.args.get('user', "user")
-    pwd = request.args.get('pwd', '"pwd"')
+    user = request.args.get('user', 'user')
+    pwd = request.args.get('pwd', 'pwd')
     #pwd_c = request.args.get('pwd_c', '"pwd_c"')
     email = request.args.get('email', "email")
     SN = request.args.get('SN', "SN")
@@ -25,8 +28,7 @@ def signup_confirm():
 
 
 def getPw(id_):
-    print(id_)
-    cur.execute(f"SELECT pw FROM sign WHERE id={id_}")
+    cur.execute(f'SELECT pw FROM sign WHERE id="{id_}"')
     #cur.execute(f"SELECT pw FROM sign")
     rows = cur.fetchall()
     return rows[0][0]
@@ -34,10 +36,8 @@ def getPw(id_):
 
 @app.route('/login_confirm', methods=['GET', 'POST'])
 def login_confirm():
-    user = request.args.get('user', "user")
-    pwd = request.args.get('pwd', '"pwd"')
-    pwd = pwd.strip('"')
-    print(user, pwd)
+    user = request.args.get('user', 'user')
+    pwd = request.args.get('pwd', 'pwd')
     if pwd == getPw(user):
         session['user'] = user
         temp = {"user" : user, "login" : "True"}
@@ -66,11 +66,16 @@ def login():
 def signup():
     return render_template('signup.html')
 
-#@app.route('/admin')
+@app.route('/admin')
 def admin():
     cur.execute("SELECT * FROM sign")
     rows = cur.fetchall()
-    return rows
+    return jsonify(rows)
+
+@app.route('/remove')
+def remove():
+    cur.execute("DELETE FROM sign")
+    return "done"
 
 
 
